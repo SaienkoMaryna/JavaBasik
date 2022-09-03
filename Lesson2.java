a35713c7386ec0cca0297f596674b4ff0ee018a7
SaienkoMaryna <111528006+SaienkoMaryna@users.noreply.github.com>
2022-09-03 15:47:23 +0200

    Create Lesson2.java

---
+++ Lesson2.java
@@ -0,0 +1,18 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+            int x = 20;
+            int y = 20;
+            int z = x - y;
+            int k = x + y;
+            int u = x / y;
+            int b = x%y;
+            int a = x * y;
+            System.out.println(z);
+            System.out.println(k);
+            System.out.println(u);
+            System.out.println(b);
+            System.out.println(a);
+
+        }
+
+    }
