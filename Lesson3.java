8598f27a2f62e18b1b3f79047a0031cdfb845c60
SaienkoMaryna <111528006+SaienkoMaryna@users.noreply.github.com>
2022-09-05 20:29:27 +0200

    Create Lesson3.java

---
+++ Lesson3.java
@@ -0,0 +1,29 @@
+class Lesson3 {
+    public static void main (String[] args) {
+        //integer types : int, byte, short, long
+        //int b,c,d,e; 
+        int a;
+        a = 10;
+        System.out.println(a);
+        // non integer types: float, double
+        float b = 5 / 2f;
+        System.out.println (b);
+        // characters: characters
+        char c = 'q';
+        System.out.println(c);
+        // boolean type : boolean 
+        boolean f = true;
+        System.out.println(f);
+        // arrays is references type
+        int[] array = new int[24];
+        array[0] = 11;
+        System.out.println(array[0]);
+        float x = a * 3 + b;
+        System.out.println(x);
+        System.out.println(Math.PI);
+        f = a > 10;
+        System.out.println(f);
+    
+        
+}
+}
